4b760c47273f18f17e335dd24d76d0c38ee5f380
wcyCoulson <43095445+wcyCoulson@users.noreply.github.com>
2019-07-18 09:14:33 +0800

    homework6

---
+++ todolist/app/src/main/java/com/byted/camp/todolist/beans/Priority.java
@@ -0,0 +1,19 @@
+package com.byted.camp.todolist.beans;
+
+import android.graphics.Color;
+
+public enum Priority {
+//    public final int value;
+//    public final int color;
+//
+//    Priority( int value , int color){
+//        this.value = value;
+//        this.color = color;
+//    }
+//
+//    public static Priority from(int value){
+//
+//    }
+
+
+}
